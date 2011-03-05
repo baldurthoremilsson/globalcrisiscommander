d07bae49fb17cdae2a944e1ba3b92d1a644e2b6f
Baldur Þór Emilsson <baldur@baldur.biz>
2011-03-05 15:57:21 +0000

    fullscreen map

--- gcc.js
+++ gcc.js
@@ -5,5 +5,5 @@ $(function() {
         map.setUIToDefault();
     }
     $("#map_canvas").height($(window).height()-200);
-    $(window).resize($("#map_canvas").height($("#map_canvas").height($(window).height()-200));
+    $(window).resize($("#map_canvas").height($("#map_canvas").height($(window).height()-200)));
 });
