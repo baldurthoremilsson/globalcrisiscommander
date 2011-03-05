43aa3fdb086b04485b3de50079797758cf2ddafe
Baldur Þór Emilsson <baldur@baldur.biz>
2011-03-05 21:12:50 +0000

    sumthin

--- gcc.js
+++ gcc.js
@@ -1,3 +1,56 @@
+var gcc = {
+    accidents: [{
+        location: new google.maps.LatLng(48.855801, 2.324073),
+        type: "fire",
+        title: "Fire at the bakery"
+    }],
+    stations: [{
+        location: new google.maps.LatLng(48.861815, 2.343256),
+        type: "firestation"
+    }]
+};
+
+var icons = {
+    accidents: {
+        fire: "assets/pics/fire-icon.png"
+    },
+    stations: {
+        firestation: "assets/pics/firestation.png"
+    }
+}
+
+function addAccident(accident, map) {
+    var marker = new google.maps.Marker({
+        position: accident.location,
+        map: map,
+        title: accident.title,
+        icon: icons.accidents[accident.type]
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        $("#dock").empty();
+        var dockitem = $('<div class="dockitem">THERE IS A FIRE</div>').droppable({
+            drop: function( event, ui ) {
+                alert('YOU WIN');
+            }
+        });
+        $("#dock").append(dockitem);
+    });
+}
+
+function addStation(station, map) {
+    var marker = new google.maps.Marker({
+        position: station.location,
+        map: map,
+        title: station.title,
+        icon: icons.stations[station.type]
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        $("#sidebar").empty();
+        var sidebaritem = $('<div class="sidebaritem">FIRETRUCK</div>').draggable({revert: true});
+        $("#sidebar").append(sidebaritem);
+    });
+}
+
 $(function() {
     var myLatlng = new google.maps.LatLng(48.860706, 2.341182);
     var myOptions = {
@@ -13,19 +66,12 @@ $(function() {
         $("#map_canvas").height($("#map_canvas").height($(window).height()-MENU_SIZE));
     });
     
-    var markerPos = new google.maps.LatLng(48.855801, 2.324073);
-    var marker = new google.maps.Marker({
-        position: markerPos,
-        map: map,
-        title: 'FIRE AT THE BAKERY',
-        icon: "assets/pics/fire-icon.png",
-    });
-    
-    google.maps.event.addListener(marker, 'click', function() {
-        alert('sprell');
-    });
-    
-    $(".dockitem").draggable();
+    for(i = 0; i < gcc.accidents.length; i++) {
+        addAccident(gcc.accidents[i], map);
+    }
+    for(i = 0; i < gcc.stations.length; i++) {
+        addStation(gcc.stations[i], map);
+    }
 });
 
 function dockItem() {
