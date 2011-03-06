6b730e9a2b47c9f6540bf07e7a846036fad6f765
Björn Guðmundsson <lettfeti@gmail.com>
2011-03-06 20:56:52 +0000

    a static way to put up a popup

---
+++ popup.js
@@ -0,0 +1,70 @@
+﻿var popupStatus = 0;
+
+function loadPopup(){
+	if(popupStatus==0){
+		$("#backgroundPopup").css({
+			"opacity": "0.7"
+		});
+		$("#backgroundPopup").fadeIn("slow");
+		$("#popupContact").fadeIn("slow");
+		popupStatus = 1;
+	}
+}
+
+function disablePopup(){
+	if(popupStatus==1){
+		$("#backgroundPopup").fadeOut("slow");
+		$("#popupContact").fadeOut("slow");
+		popupStatus = 0;
+	}
+}
+
+function centerPopup(){
+	var windowWidth = document.documentElement.clientWidth;
+	var windowHeight = document.documentElement.clientHeight;
+	var popupHeight = $("#popupContact").height();
+	var popupWidth = $("#popupContact").width();
+	//centering
+	$("#popupContact").css({
+		"position": "absolute",
+		"top": windowHeight/2-popupHeight/2,
+		"left": windowWidth/2-popupWidth/2
+	});
+
+	
+}
+
+
+//CONTROLLING EVENTS IN jQuery
+$(document).ready(function(){
+	//Auto popup
+	centerPopup();
+	loadPopup();
+	//LOADING POPUP
+	//Click the button event!
+	/*
+	$("#button").click(function(){
+		//centering with css
+		centerPopup();
+		//load popup
+		loadPopup();
+	});
+	*/
+				
+	//CLOSING POPUP
+	//Click the x event!
+	$("#popupContactClose").click(function(){
+		disablePopup();
+	});
+	//Click out event!
+	$("#backgroundPopup").click(function(){
+		disablePopup();
+	});
+	//Press Escape event!
+	$(document).keypress(function(e){
+		if(e.keyCode==27 && popupStatus==1){
+			disablePopup();
+		}
+	});
+
+});
