084974f148f646554339fe93517d913a6936b73a
gunnarrb <gunnarrb@gmail.com>
2011-03-20 23:52:03 +0000

    hackaround the stacking animations bug

--- gcc.js
+++ gcc.js
@@ -70,9 +70,9 @@ function addStation(station, map) {
         $("#sidebar").empty();
         var sidebaritem = $('<div class="sidebaritem"><div id="item_desc">Ble</div><div id="item_image"><img class="accident" src="assets/pics/firestation.png\></div></div>').draggable({revert: true}).mouseenter(function() {
             $(this).find("#item_image").css({"opacity":"0.6"});
-            $(this).find("#item_desc").fadeIn();
+            $(this).find("#item_desc").fadeIn('fast');
         }).mouseleave(function() { 
-            $(this).find("#item_desc").fadeOut();
+            $(this).find("#item_desc").fadeOut('fast',function(){enableAnimation = true;});
             $(this).find("#item_image").css({"opacity":"1.0"});
 
         });
