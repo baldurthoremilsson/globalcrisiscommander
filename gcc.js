17c9603ddebc2e77abbef5403087525f6a89921d
Baldur Þór Emilsson <baldur@baldur.biz>
2011-03-23 21:14:19 +0000

    refactoring

--- gcc.js
+++ gcc.js
@@ -1,3 +1,37 @@
+(function($){
+     $.fn.extend({
+          center: function (options) {
+               var options =  $.extend({ // Default values
+                    inside:window, // element, center into window
+                    transition: 0, // millisecond, transition time
+                    minX:0, // pixel, minimum left element value
+                    minY:0, // pixel, minimum top element value
+                    vertical:true, // booleen, center vertical
+                    withScrolling:true, // booleen, take care of element inside scrollTop when minX < 0 and window is small or when window is big
+                    horizontal:true // booleen, center horizontal
+               }, options);
+               return this.each(function() {
+                    var props = {position:'absolute'};
+                    if (options.vertical) {
+                         var top = ($(options.inside).height() - $(this).outerHeight()) / 2;
+                         if (options.withScrolling) top += $(options.inside).scrollTop() || 0;
+                         top = (top > options.minY ? top : options.minY);
+                         $.extend(props, {top: top+'px'});
+                    }
+                    if (options.horizontal) {
+                          var left = ($(options.inside).width() - $(this).outerWidth()) / 2;
+                          if (options.withScrolling) left += $(options.inside).scrollLeft() || 0;
+                          left = (left > options.minX ? left : options.minX);
+                          $.extend(props, {left: left+'px'});
+                    }
+                    if (options.transition > 0) $(this).animate(props, options.transition);
+                    else $(this).css(props);
+                    return $(this);
+               });
+          }
+     });
+})(jQuery);
+
 var gcc = {
     levels: [
         {
@@ -31,8 +65,7 @@ var gcc = {
                 description: "There has been a three car crash on Hollister Avenue, Santa Barbara. Two people are injured, one of which is trapped in their car. Traffic is jammed near the accident."
             }
         }
-    ],
-    
+    ]
 };
 
 // Game
@@ -41,106 +74,111 @@ gcc.Game = function(id) {
     
     this.DOM.board = $("#" + id);
     
-    this.DOM.mapDock
-        .append(this.DOM.map)
-        .append(this.DOM.dock);
     this.DOM.board
-        .append(this.DOM.mapDock)
+        .append(this.DOM.map)
+        .append(this.DOM.dock)
         .append(this.DOM.sidebar);
     
     this.map = new google.maps.Map(this.DOM.map[0], this.mapOptions);
     this.accidents = [];
     this.stations = [];
     
-    this.DOM.map
-        .height($(window).height()-this.MENU_SIZE)
-        .width($(window).width()-this.MENU_SIZE);
     $(window).resize(function() {
         self.DOM.map
-            .height($(window).height()-self.MENU_SIZE)
+            .height($(window).height()-self.MENU_SIZE+2)
+            .width($(window).width()-self.MENU_SIZE+2);
+        self.DOM.sidebar
+            .height($(window).height())
+            .width(self.MENU_SIZE);
+        self.DOM.dock
+            .height(self.MENU_SIZE)
             .width($(window).width()-self.MENU_SIZE);
         self.map.setCenter(self.location);
     });
+    $(window).resize();
 };
 
-gcc.Game.prototype = {
-    DOM: {
-        map: $('<div id="map_canvas"></div>'),
-        dock: $('<div id="dock"></div>'),
-        sidebar: $('<div id="sidebar"></div>'),
-        mapDock: $('<div id="map_dock"></div>')
-    },
-    mapOptions: {
-        zoom: 16,
-        center: new google.maps.LatLng(0, 0),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    },
-    MENU_SIZE: 152,
-    
-    
-    startLevel: function(level) {
-        this.accidents = [];
-        this.stations = [];
+    gcc.Game.prototype = {
+        DOM: {
+            map: $('<div class="map_canvas"></div>'),
+            dock: $('<div class="dock"></div>'),
+            sidebar: $('<div class="sidebar"></div>')
+        },
+        mapOptions: {
+            zoom: 16,
+            center: new google.maps.LatLng(0, 0),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        MENU_SIZE: 150,
+        INFOBOX_RATIO: 0.75,
         
-        this.map.setCenter(new google.maps.LatLng(level.location.lat, level.location.long));
         
-        for(i in level.accidents)
-            this.addAccident(level.accidents[i]);
+        startLevel: function(level) {
+            this.accidents = [];
+            this.stations = [];
+            
+            this.map.setCenter(new google.maps.LatLng(level.location.lat, level.location.long));
+            
+            for(i in level.accidents)
+                this.addAccident(level.accidents[i]);
+            
+            for(i in level.stations)
+                this.addStation(level.stations[i]);
+            
+            this.displayAccidents();
+            this.displayStations();
+        },
+        addUnit: function(unit) {
+        },
+        addAccident: function(accident) {
+            var acc = new gcc.Accident(accident);
+            this.accidents.push(acc);
+            
+            acc.marker.setMap(this.map);
+        },
+        addStation: function(station) {
+            var st = new gcc.Station(station);
+            this.stations.push(st);
+            
+            st.marker.setMap(this.map);
+        },
+        pause: function() {
+        },
+        play: function() {
+        },
         
-        for(i in level.stations)
-            this.addStation(level.stations[i]);
-        
-        this.displayAccidents();
-        this.displayStations();
-    },
-    addUnit: function(unit) {
-    },
-    addAccident: function(accident) {
-        var acc = new gcc.Accident(accident);
-        this.accidents.push(acc);
-        
-        acc.marker.setMap(this.map);
-    },
-    addStation: function(station) {
-        var st = new gcc.Station(station);
-        this.stations.push(st);
-        
-        st.marker.setMap(this.map);
-    },
-    pause: function() {
-    },
-    play: function() {
-    },
-    
-    displayAccidents: function() {
-        var dock = this.DOM.dock;
-        
-        dock.empty();
-        for(i in this.accidents) {
-            // FIXME: this logic should be in the Accident constructor
-            var acc = this.accidents[i];
-            acc.DOM
-                .mouseenter(acc.mouseenter)
-                .mouseleave(acc.mouseleave)
-                .click(acc.click);
-            dock.append(acc.DOM);
+        displayAccidents: function() {
+            var dock = this.DOM.dock;
+            
+            dock.empty();
+            for(i in this.accidents) {
+                // FIXME: this logic should be in the Accident constructor
+                var acc = this.accidents[i];
+                acc.DOM
+                    .data("accident", acc)
+                    .click(function() {
+                        $(this).data("accident").click();
+                    });
+                dock.append(acc.DOM);
+            }
+        },
+        displayStations: function() {
+            var sidebar = this.DOM.sidebar;
+            
+            sidebar.empty();
+            for(i in this.stations) {
+                var st = this.stations[i];
+                // FIXME: this logic should be in the Station constructor
+                st.DOM
+                    .data("station", st)
+                    .click(function() {
+                        $(this).data("station").click();
+                    });
+                sidebar.append(st.DOM);
+            }
         }
-    },
-    displayStations: function() {
-        var sidebar = this.DOM.sidebar;
-        
-        sidebar.empty();
-        for(i in this.stations) {
-            var st = this.stations[i];
-            // FIXME: this logig should be in the Station constructor
-            st.DOM
-                .mouseenter(st.mouseenter)
-                .mouseleave(st.mouseleave)
-                .click(st.click);
-            sidebar.append(st.DOM);
-        }
-    }
-}
+    };
+
 
 // Accidents
 gcc.Accident = function(accident) {
@@ -166,7 +204,7 @@ gcc.Accident = function(accident) {
             break;
     }
     
-    this.DOM = $('<div class="dockitem"><div id="item_desc">accident</div><div id="item_image"><img class="accident" src="' + dockImage + '"></div></div>');
+    this.DOM = gcc.getInfobox("dock", "accident", dockImage);
     
     this.marker = new google.maps.Marker({
         position: this.location,
@@ -175,42 +213,33 @@ gcc.Accident = function(accident) {
     google.maps.event.addListener(this.marker, 'click', function() {
         self.click();
     });
-}
+};
 
-gcc.Accident.prototype = {
-    mouseenter: function() {
-        $(this).find("#item_image").css({"opacity":"0.6"});
-        $(this).find("#item_desc").fadeIn();
-    },
-    mouseleave: function() {
-        $(this).find("#item_desc").fadeOut();
-        $(this).find("#item_image").css({"opacity":"1.0"});
-    },
-    click: function() {
-        var link = $('<a href="#">Back</a>').click(function() {
-                gcc.game.displayAccidents();
-            });
-        gcc.game.DOM.dock.empty().append(link);
-        /*
-        var dock = gcc.game.DOM.dock,
-            dockitem = this.DOM,
-        // Place the image in the center
-            width = dockitem.width();
-            height = dockitem.height();
-            imgwidth = dockitem.find("img").width();
-            imgheight = dockitem.find("img").height();
-        
-        dockitem.find("img").css({"top":height/2-imgheight/2, "left":width/2-imgwidth/2});
-        
-        dock.empty().append(dockitem);
-        */
-    }
-}
+    gcc.Accident.prototype = {
+        click: function() {
+            var link = $('<a href="#">Back</a>').click(function() {
+                    gcc.game.displayAccidents();
+                });
+            gcc.game.DOM.dock.empty().append(link);
+            for(i in this.incidents) {
+                var incident = this.incidents[i];
+                incident.DOM
+                    .data("incident", incident)
+                    .droppable();
+                gcc.game.DOM.dock.append(this.incidents[i].DOM);
+            }
+            
+            return false;
+        }
+    };
 
 gcc.Station = function(station) {
     var self = this,
         dockImage,
-        markerImage;
+        markerImage,
+        description,
+        unit;
+        i;
     
     this.location = new google.maps.LatLng(station.location.lat, station.location.long);
     this.type = station.type;
@@ -219,18 +248,21 @@ gcc.Station = function(station) {
         case "firestation":
             dockImage = "assets/pics/firestation-icon.png";
             markerImage = "assets/pics/firestation-icon.png";
+            description = "Firestation";
             break;
         case "policestation":
             dockImage = "assets/pics/policestation-icon.png";
             markerImage = "assets/pics/policestation-icon.png";
+            description = "Policestation";
             break;
         case "hospital":
             dockImage = "assets/pics/hospital-icon.png";
             markerImage = "assets/pics/hospital-icon.png";
+            description = "Hospital";
             break;
     }
     
-    this.DOM = $('<div class="sidebaritem"><div id="item_desc">accident</div><div id="item_image"><img class="accident" src="' + dockImage + '"></div></div>');
+    this.DOM = this.DOM = gcc.getInfobox("sidebar", "station", dockImage);
     
     this.marker = new google.maps.Marker({
         position: this.location,
@@ -239,186 +271,88 @@ gcc.Station = function(station) {
     google.maps.event.addListener(this.marker, 'click', function() {
         self.click();
     });
-}
+    
+    this.units = [];
+    for(i = 0; i < station.units; i++)
+        this.units.push(new gcc.Unit(this));
+};
 
-gcc.Station.prototype = {
-    mouseenter: function() {
-        $(this).find("#item_image").css({"opacity":"0.6"});
-        $(this).find("#item_desc").fadeIn();
-    },
-    mouseleave: function() {
-        $(this).find("#item_desc").fadeOut();
-        $(this).find("#item_image").css({"opacity":"1.0"});
-    },
-    click: function() {
-        var link = $('<a href="#">Back</a>').click(function() {
-                gcc.game.displayStations();
-            });
-        gcc.game.DOM.sidebar.empty().append(link);
-        /*
-        var dock = gcc.game.DOM.dock,
-            dockitem = this.DOM,
-        // Place the image in the center
-            width = dockitem.width();
-            height = dockitem.height();
-            imgwidth = dockitem.find("img").width();
-            imgheight = dockitem.find("img").height();
-        
-        dockitem.find("img").css({"top":height/2-imgheight/2, "left":width/2-imgwidth/2});
-        
-        dock.empty().append(dockitem);
-        */
-    }
-}
+    gcc.Station.prototype = {
+        click: function() {
+            var link = $('<a href="#">Back</a>').click(function() {
+                    gcc.game.displayStations();
+                });
+            gcc.game.DOM.sidebar.empty().append(link);
+            
+            for(i in this.units) {
+                var unit = this.units[i];
+                unit.DOM
+                    .data("unit", unit)
+                    .draggable(unit.draggable);
+                gcc.game.DOM.sidebar.append(unit.DOM);
+            }
+            
+            return false;
+        }
+    };
+
+gcc.Incident = function(accident) {
+    this.accident = accident;
+    this.DOM = gcc.getInfobox("dock", "incident", "");
+};
+	gcc.Incident.prototype = {
+	};
+
+gcc.Unit = function(station) {
+    var self = this;
+    
+    this.station = station;
+    this.DOM = gcc.getInfobox("sidebar", "unit", "");
+};
+	gcc.Unit.prototype = {
+	};
+
+gcc.getInfobox = function(type, className, img) {
+	var infoBox,
+		infoBoxClass,
+		width,
+		height;
+	
+	switch(type) {
+		case "dock":
+			infoBoxClass = "dockitem";
+			height = this.game.MENU_SIZE - 24;
+			width = height * this.game.INFOBOX_RATIO;
+			break;
+		case "sidebar":
+			infoBoxClass = "sidebaritem";
+			width = this.game.MENU_SIZE - 24;
+			height = width * this.game.INFOBOX_RATIO;
+			break;
+	}
+	
+	infoBox = $(
+		'<div class="' + infoBoxClass + " " + className + '">' +
+			'<img src="' + img + '">' +
+			'<div class="description">' +
+				'<div class="text">' +
+					'description' +
+				'</div>' +
+			'<div class="background"></div>' +
+		'</div>'
+	).width(width).height(height);
+	
+	$('img', infoBox).load(function() {
+		$(this).center({
+			inside: infoBox,
+			minX: -$(this).width()/2,
+			minY: -$(this).height()/2});
+	});
+	
+	return infoBox;
+};
 
 $(function() {
     gcc.game = new gcc.Game("board");
     gcc.game.startLevel(gcc.levels[0]);
 });
-
-/*
-var icons = {
-    accidents: {
-        fire: "assets/pics/fire-icon.png"
-    },
-    stations: {
-        firestation: "assets/pics/firestation-icon.png"
-    }
-}
-
-var enableAnimation = true;
-
-function addAccident(accident, map) {
-    var marker = new google.maps.Marker({
-        position: accident.location,
-        map: map,
-        title: accident.title,
-        icon: icons.accidents[accident.type]
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        $("#dock").empty();
-        var dockitem = $('<div class="dockitem"><div id="item_desc">Ble</div><div id="item_image"><img class="accident" src="assets/pics/fire.jpg\></div></div>').droppable({
-            drop: function( event, ui ) {
-                alert('YOU WIN');
-            }
-        }).mouseenter(function() {
-            if(enableAnimation) 
-            {
-                enableAnimation = false;
-                $(this).find("#item_image").css({"opacity":"0.6"});
-                $(this).find("#item_desc").fadeIn('fast');
-            }
-        }).mouseleave(function() { 
-            $(this).find("#item_desc").fadeOut('fast',function(){enableAnimation = true;});
-            $(this).find("#item_image").css({"opacity":"1.0"});
-
-        });
-        // Place the image in the center
-        var width = $(dockitem).width();
-        var height = $(dockitem).height();
-        var imgwidth = $(dockitem).find("img").width();
-        var imgheight = $(dockitem).find("img").height();
-        $(dockitem).find("img").css({"top":height/2-imgheight/2, "left":width/2-imgwidth/2});
-        $("#dock").append(dockitem);
-    });
-}
-
-
-
-
-function addStation(station, map) {
-    var marker = new google.maps.Marker({
-        position: station.location,
-        map: map,
-        title: station.title,
-        icon: icons.stations[station.type]
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        $("#sidebar").empty();
-        var sidebaritem = $('<div class="sidebaritem"><div id="item_desc">Ble</div><div id="item_image"><img class="accident" src="assets/pics/firestation.png\></div></div>').draggable({revert: true}).mouseenter(function() {
-            if(enableAnimation) 
-            {
-                enableAnimation = false;
-                $(this).find("#item_image").css({"opacity":"0.6"});
-                $(this).find("#item_desc").fadeIn('fast');
-            }
-        }).mouseleave(function() { 
-            $(this).find("#item_desc").fadeOut('fast',function(){enableAnimation = true;});
-            $(this).find("#item_image").css({"opacity":"1.0"});
-
-        });
-   // Place the image in the center
-        var width = $(sidebaritem).width();
-        var height = $(sidebaritem).height();
-        var imgwidth = $(sidebaritem).find("img").width();
-        var imgheight = $(sidebaritem).find("img").height();
-        $(sidebaritem).find("img").css({"top":height/2-imgheight/2, "left":width/2-imgwidth/2});
-        $("#sidebar").append(sidebaritem);
-    });
-}
-
-$(function() {
-    var myLatlng = new google.maps.LatLng(48.860706, 2.341182);
-    var myOptions = {
-        zoom: 15,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    
-    var MENU_SIZE = 152;
-    $("#map_canvas")
-        .height($(window).height()-MENU_SIZE)
-        .width($(window).width()-MENU_SIZE);
-    $(window).resize(function() {
-        $("#map_canvas")
-            .height($(window).height()-MENU_SIZE)
-            .width($(window).width()-MENU_SIZE);
-    });
-    
-    for(i = 0; i < gcc.accidents.length; i++) {
-        addAccident(gcc.accidents[i], map);
-    }
-    for(i = 0; i < gcc.stations.length; i++) {
-        addStation(gcc.stations[i], map);
-    }
-});
-
-function dockItem() {
-    
-}
-
-$().ready(function(){
-	//SETUP Mp3 files player example
-	var playerMp3 = new SWFObject("player.swf","myplayer1","0","0","9");
-	//playerMp3.addVariable("logo","css/images/logo.jpg");
-	playerMp3.addVariable("file","assets/songs/BraveHeart.mp3");
-	playerMp3.addVariable("icons","false");
-	playerMp3.write("player1");
-	//create a javascript object to allow us send events to the flash player
-	var player1 = document.getElementById("myplayer1");
-	var mute1 = 0;
-	var status1 = $("#status1");
-	
-	//EVENTS for Mp3 files player
-	$("#play1").click(function(){
-		player1.sendEvent("PLAY","true");
-		status1.text("PLAYING...");
-	});
-	$("#pause1").click(function(){
-		player1.sendEvent("PLAY","false");
-		status1.text("PAUSED");
-	});
-	$("#mute1").click(function(){
-		if(mute1 == 0){
-			player1.sendEvent("mute","true");
-			mute1 = 1;
-		}
-		else{
-			player1.sendEvent("mute","false");
-			mute1 = 0;
-		}
-	});
-});
-
-*/
