e0185dd5c6cadfda0a451729d287293785dbf06e
Baldur Þór Emilsson <baldur@baldur.biz>
2011-04-04 01:25:23 +0000

    Removed unused Array extension

--- gcc.js
+++ gcc.js
@@ -1,11 +1,3 @@
-// Array Remove - By John Resig (MIT Licensed)
-// http://ejohn.org/blog/javascript-array-remove/
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
-
 // http://plugins.jquery.com/project/autocenter
 (function($){
      $.fn.extend({
