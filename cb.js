35763d864381b0be34078d56af397a24b3ece8f2
gunnarrb <gunnarrb@gmail.com>
2011-04-19 11:49:22 +0000

    control box now has jplayer

--- cb.js
+++ cb.js
@@ -13,12 +13,43 @@ var controlBoxObject =
 						'<div class="scoretext cbc">Level</div><div class="score cbc">1</div>'+
 						'<div class="timertext cbc">Time</div><div class="timer cbc">0</div>'+
 						'<div class="leveltext cbc">Score</div><div class="level cbc">0</div>'+
+
 					'</div>'+
-					
 					'<div id="controlboxExpanded" style="display: none;">'+
-						'<div class="expanded">'+
+					'<div id="jquery_jplayer_1" class="jp-jplayer"></div>'+
+					'<div class="jp-audio">'+
+						'<div class="jp-type-single">'+
+							'<div class="jp-interface" id="jp_interface_1">'+
+								'<ul class="jp-controls">'+
+									'<li><a tabindex="1" class="jp-play" href="#" style="display: block;">play</a></li>'+
+									'<li><a tabindex="1" class="jp-pause" href="#" style="display: none;">pause</a></li>'+
+									'<li><a tabindex="1" class="jp-stop" href="#">stop</a></li>'+
+									'<li><a tabindex="1" class="jp-mute" href="#">mute</a></li>'+
+									'<li><a tabindex="1" class="jp-unmute" href="#" style="display: none;">unmute</a></li>'+
+								'</ul>'+
+								'<div class="jp-progress">'+
+									'<div class="jp-seek-bar" style="width: 100%;">'+
+										'<div class="jp-play-bar" style="width: 2.07816%;"></div>'+
+									'</div>'+
+								'</div>'+
+								'<div class="jp-volume-bar">'+
+									'<div class="jp-volume-bar-value" style="width: 80%;"></div>'+
+								'</div>'+
+								'<div class="jp-current-time"></div>'+
+								'<div class="jp-duration"></div>'+
+							'</div>'+ 
+							'<!--'+
+							'<div class="jp-playlist" id="jp_playlist_1">'+
+								'<ul>'+
+									'<li></li>'+
+								'</ul>'+
+							'</div>'+
+							'-->'+
 						'</div>'+
 					'</div>'+
+					
+					'</div>'+
+		
 				 '</div>'+
 				'<div id="overlay"></div>'
 			),
@@ -49,10 +80,10 @@ var controlBoxObject =
 			$('#controlbox .arrow-down').fadeOut();
 			$('#controlbox #controlboxCompressed').fadeOut('fast');
 			$('#overlay').animate(
-				{width: 500, height: 100},
+				{width: 400, height: 80},
 				200,
 				function() { $('#controlbox #controlboxExpanded').fadeIn('fast');
-							 $('#controlbox').css({width: 500, height: 100});
+							 $('#controlbox').css({width: 400, height: 80});
 							 $('#controlbox .arrow-up').fadeIn();
 						   } 
 			);
